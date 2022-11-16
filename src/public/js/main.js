40fdc634b85153bc91cf17113deae0b89f966ae1
yossgar <112755243+yossgar@users.noreply.github.com>
2022-11-16 17:08:21 +0200

    fixes to index page

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,3 +2,21 @@ function toggleTooltip() {
   var tooltip = document.getElementById("tooltip");
   tooltip.classList.toggle("d-none");
 }
+
+$(() => {
+  console.log("all user logout links: ", $(".logoutUser"));
+
+  if ($(".logoutUser").length > 0) {
+    $(".logoutUser").click((e) => {
+      e.preventDefault();
+      $.ajax({
+        url: "/user/logout",
+        type: "GET",
+        success: (data) => {
+          localStorage.clear();
+          location.reload();
+        },
+      });
+    });
+  }
+});
